7fd32a67584ca2381abefcb68fa3d0b868365099
Anastasiia Petrych <a.petrych@gmail.com>
2022-09-22 18:05:17 +0200

    Revert "Removed Spring profile on ScreenshotServiceGCP thus setting the service to be run by default"
    This reverts commit e3cdf6210fc965f81321324b350d5951bd2d38d4.

--- src/main/java/com/petrych/screenshotter/service/ScreenshotServiceGCP.java
+++ src/main/java/com/petrych/screenshotter/service/ScreenshotServiceGCP.java
@@ -17,6 +17,7 @@ import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @Service
+@Profile("gcp")
 class ScreenshotServiceGCP extends AbstractScreenshotService {
 	
 	private static final Logger LOG = LoggerFactory.getLogger(ScreenshotServiceGCP.class);
